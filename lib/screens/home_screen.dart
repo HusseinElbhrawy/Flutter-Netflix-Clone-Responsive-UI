847d429cf07b6e1dc54b945a32959ba89b9518a8
Hussein M Elbhrway <husseinelbhrawy@gmail.com>
2022-03-05 11:36:29 +0200

    FAB + Content List

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -44,6 +44,11 @@ class _HomeScreenState extends State<HomeScreen> {
         preferredSize: Size(screenSize.width, 50),
         child: CustomAppBar(scrollOffset: _scrollOffset),
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey.shade800,
+        onPressed: () => print('Cast'),
+        child: const Icon(Icons.cast),
+      ),
       body: CustomScrollView(
         controller: scrollController,
         slivers: [
@@ -61,8 +66,89 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
+          SliverPadding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: ContentList(
+                title: 'My List',
+                contentList: myList,
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: ContentList(
+                  title: 'Netflix Originals',
+                  contentList: originals,
+                  isOriginals: true),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: ContentList(
+                title: 'Trending',
+                contentList: trending,
+              ),
+            ),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class ContentList extends StatelessWidget {
+  const ContentList(
+      {Key? key,
+      required this.contentList,
+      required this.title,
+      this.isOriginals = false})
+      : super(key: key);
+  final String title;
+  final bool isOriginals;
+  final List<Content> contentList;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            title,
+            style: Theme.of(context).textTheme.headline6!.copyWith(
+                  color: Colors.white,
+                ),
+          ),
+        ),
+        SizedBox(
+          height: isOriginals ? 500 : 220,
+          child: ListView.builder(
+            itemCount: contentList.length,
+            scrollDirection: Axis.horizontal,
+            padding:
+                const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () => print(contentList[index].titleImageUrl),
+                child: Container(
+                  margin: const EdgeInsets.all(8.0),
+                  height: isOriginals ? 400 : 200,
+                  width: isOriginals ? 200 : 130,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(contentList[index].imageUrl),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
